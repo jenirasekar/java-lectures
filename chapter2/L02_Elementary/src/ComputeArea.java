8fe799bfae4ae5874e537b676a0a55d1985bdbb5
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-02-26 08:40:27 +0800

    first commit

---
+++ chapter2/L02_Elementary/src/ComputeArea.java
@@ -0,0 +1,12 @@
+public class ComputeArea {
+    public static void main(String[] args) {
+        double radius;
+        double area;
+
+        radius = 20;
+
+        area = radius * radius * 3.14159;
+
+        System.out.println("The are of the circle of radius " + radius + " is " + area);
+    }
+}
