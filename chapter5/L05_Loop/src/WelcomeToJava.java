5e92bd801e8d5e05591e6c0fd7e946481b35e7c3
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-03-19 08:46:08 +0800

    loop

---
+++ chapter5/L05_Loop/src/WelcomeToJava.java
@@ -0,0 +1,10 @@
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 100) {
+            System.out.println("Welcome to Java");
+            count++;
+        }
+    }
+}
