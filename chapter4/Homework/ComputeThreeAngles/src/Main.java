5bb3648962e7bd4ba4156a9d0e3a04b80bfdd182
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-03-26 08:36:48 +0800

    dep changes

--- chapter4/Homework/ComputeThreeAngles/src/Main.java
+++ chapter4/Homework/ComputeThreeAngles/src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ComputeAngles {
+public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -14,19 +14,19 @@ public class ComputeAngles {
 
         // compute three sides
         double a = Math.sqrt((x2 - x3) * (x2 - x3)
-        + (y2 - y3) * (y2 - y3));
+                + (y2 - y3) * (y2 - y3));
         double b = Math.sqrt((x1 - x3) * (x1 - x3)
-        + (y1 - y3) * (y1 - y3));
+                + (y1 - y3) * (y1 - y3));
         double c = Math.sqrt((x1 - x2) * (x1 - x2)
-        + (y1 - y2) * (y1 - y2));
+                + (y1 - y2) * (y1 - y2));
 
         // compute three angles
         double A = Math.toDegrees(Math.acos((a * a - b * b - c * c)
-        / (-2 * b * c)));
+                / (-2 * b * c)));
         double B = Math.toDegrees(Math.acos((b * b - a * a - c * c)
-        / (-2 * a * c)));
+                / (-2 * a * c)));
         double C = Math.toDegrees(Math.acos((c * c - b * b - a * a)
-        / (-2 * a * b)));
+                / (-2 * a * b)));
 
         // display results
         double sum =  A + B + C;
