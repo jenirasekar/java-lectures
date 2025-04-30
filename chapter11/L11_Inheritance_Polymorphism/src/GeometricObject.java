e9f6a3668d3da19bac0613142dce0eba73c7e3fa
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-04-30 08:44:49 +0800

    inheritance

---
+++ chapter11/L11_Inheritance_Polymorphism/src/GeometricObject.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+
+public class GeometricObject {
+    private String color ="White";
+    private boolean filled;
+    private Date dateCreated;
+
+    public GeometricObject(){
+
+    }
+
+    public GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+        dateCreated = new Date();
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+}
